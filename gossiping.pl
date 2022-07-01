e6f69113140dca3e43280b66c9a5eb63601ccc7e
Christophe Thibaut <cthibauttof@gmail.com>
2022-07-01 21:58:12 +0200

    initial commit

---
+++ gossiping.pl
@@ -0,0 +1,41 @@
+:- module(gossiping,[stopAt/3,gossips/3]).
+
+stops(albert, 4).
+stops(barnabe, 4).
+stops(clara, 5).
+
+stop(albert, 0, fleurs).
+stop(albert, 1, planete).
+stop(albert, 2, micadia).
+stop(albert, 3, fleurs).
+
+stop(barnabe, 0, fleurs).
+stop(barnabe, 1, micadia).
+stop(barnabe, 2, fleurs).
+stop(barnabe, 3, planete).
+
+stop(clara, 0, melodie).
+stop(clara, 1, micadia).
+stop(clara, 2, fleurs).
+stop(clara, 3, melodie).
+stop(clara, 4, clocher).
+
+stopAt(Driver, Minute, Station) :-
+    stops(Driver, Stops),
+    Time is Minute mod Stops,
+    stop(Driver, Time, Station). 
+
+gossips(Driver, -1, Driver).
+
+gossips(Receiver, Minute, Gossips) :-
+    Minute >= 0,
+    stopAt(Receiver, Minute, Station),
+    findall(Driver, stopAt(Driver, Minute, Station), Drivers),
+    MinuteMinus1 is Minute - 1,
+    setof(PreviousGossips, D^ ( member(D, Drivers), gossips(D, MinuteMinus1, PreviousGossips)), AllGossips),
+    foldl(union, AllGossips, [], FoldedGossips),
+    sort(FoldedGossips, Gossips).
+
+   
+
+
---
+++ gossiping_tests.pl
@@ -0,0 +1,32 @@
+% to run the tests:
+% swipl -g run_tests -t halt gossiping_tests.pl
+
+:- begin_tests(gossiping_time).
+
+
+:- use_module('gossiping'). 
+
+test('albert at time 0 stops at station fleurs') :-
+    stopAt(albert, 0, fleurs).
+test('albert at time 5 stops at station planete') :-
+    stopAt(albert, 5, planete).
+test('barnabe at time 6 stops at station fleurs') :-
+    stopAt(barnabe, 6, fleurs).
+test('not all drivers have the same route length') :-
+    stopAt(clara, 4, clocher),
+    stopAt(clara, 5, melodie).
+test('at minute 0, albert has albert and barnabe\'s gossips') :-
+   gossips(albert,0,Gossips),  write(Gossips), nl, Gossips == [albert,barnabe].
+test('at minute 0, barnabe has albert and barnabe\'s gossips') :-
+   gossips(barnabe,0,[albert,barnabe]).
+test('at minute 0 clara has only her gossips') :-
+    gossips(clara,0,Gossips),
+    write(Gossips),
+    nl,
+    Gossips == [clara].
+test('at minute 1, clara and barnabe share their gossips from minute 0') :-
+    gossips(clara, 1, Gossips), write(Gossips), nl, Gossips == [albert,barnabe,clara].
+
+:- end_tests(gossiping_time).
+
+
